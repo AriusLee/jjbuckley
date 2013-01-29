cf956e374c332cce8d5d68df1e4d44d98b301ed8
Arius <ariusleechihin@gmail.com>
2013-01-29 21:38:22 +0800

    Add: BSSC combination layout.

---
+++ public/assets/javascripts/application.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+
+  $('#navigation a').click(function() {
+    var $target = $(this).attr('class')
+    $('body').attr('id', $target);
+    
+    switch ($target) {
+      case "jjbuckley":
+        $('.lightbox h1').html('$15 GIFT AT <a href=\"http://www.fromyouflowers.com/\">FROMYOUFLOWERS.COM</a>');
+        $('.lightbox h2').html('Compliments of JJ Buckley Fines Wines');
+        break;
+      case "bssc":
+        $('.lightbox h1').html('$15 GIFT AT <a href=\"http://www.fromyouflowers.com/\">FROMYOUFLOWERS.COM</a>');
+        $('.lightbox h2').html('Compliments of Boston Ski & Sports Club');
+        break;
+    }
+    
+  });
+  
+});
