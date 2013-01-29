77975619c2d35f20af484ced8a985de130014cb5
Arius <ariusleechihin@gmail.com>
2013-01-30 00:53:18 +0800

    Add: Winetasting combination layout.

--- public/assets/javascripts/application.js
+++ public/assets/javascripts/application.js
@@ -8,26 +8,44 @@ $(document).ready(function() {
       case "jjbuckley":
         $('.lightbox h1').html('$15 GIFT AT <a href=\"http://www.fromyouflowers.com/\">FROMYOUFLOWERS.COM</a>');
         $('.lightbox h2').html('Compliments of JJ Buckley Fines Wines');
+        $('.lightbox_decor').attr('class', 'lightbox_decor uflowers');
+        $('.advertiser_logo img').attr('src', 'assets/images/logos/uflowers.png');
+        break;
+      case "winetasting":
+        $('.lightbox h1').html('$15 GIFT AT <a href=\"http://www.livingsocial.com/\">LIVINGSOCIAL.COM</a>');
+        $('.lightbox h2').html('Compliments of WineTasting.com');
+        $('.lightbox_decor').attr('class', 'lightbox_decor livingsocial');
+        $('.advertiser_logo img').attr('src', 'assets/images/logos/livingsocial.png');
         break;
       case "bssc":
         $('.lightbox h1').html('$15 GIFT AT <a href=\"http://www.fromyouflowers.com/\">FROMYOUFLOWERS.COM</a>');
         $('.lightbox h2').html('Compliments of Boston Ski & Sports Club');
+        $('.lightbox_decor').attr('class', 'lightbox_decor uflowers');
+        $('.advertiser_logo img').attr('src', 'assets/images/logos/uflowers.png');
         break;
       case "citygolf":
         $('.lightbox h1').html('$15 GIFT AT <a href=\"http://www.fromyouflowers.com/\">FROMYOUFLOWERS.COM</a>');
         $('.lightbox h2').html('Compliments of CityGolf Boston');
+        $('.lightbox_decor').attr('class', 'lightbox_decor uflowers');
+        $('.advertiser_logo img').attr('src', 'assets/images/logos/uflowers.png');
         break;
       case "harryhasit":
         $('.lightbox h1').html('$15 GIFT AT <a href=\"http://www.fromyouflowers.com/\">FROMYOUFLOWERS.COM</a>');
         $('.lightbox h2').html('Compliments of HarryHasIt.com');
+        $('.lightbox_decor').attr('class', 'lightbox_decor uflowers');
+        $('.advertiser_logo img').attr('src', 'assets/images/logos/uflowers.png');
         break;
       case "rockspot":
         $('.lightbox h1').html('$15 GIFT AT <a href=\"http://www.fromyouflowers.com/\">FROMYOUFLOWERS.COM</a>');
         $('.lightbox h2').html('Compliments of Rock Spot Climbing');
+        $('.lightbox_decor').attr('class', 'lightbox_decor uflowers');
+        $('.advertiser_logo img').attr('src', 'assets/images/logos/uflowers.png');
         break;
       case "northern_outdoors":
         $('.lightbox h1').html('$15 GIFT AT <a href=\"http://www.fromyouflowers.com/\">FROMYOUFLOWERS.COM</a>');
         $('.lightbox h2').html('Compliments of Northern Outdoors');
+        $('.lightbox_decor').attr('class', 'lightbox_decor uflowers');
+        $('.advertiser_logo img').attr('src', 'assets/images/logos/uflowers.png');
         break;
     }
     
